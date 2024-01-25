2382a3bc458056d94b931fbb15f09f0673587dca
lenkoran26 <karnel26@gmail.com>
2024-01-25 20:37:41 +0300

    users

--- main/forms.py
+++ main/forms.py
@@ -5,3 +5,4 @@ class PostForm(forms.Form):
     author = forms.CharField(max_length=50, label='Автор')
     title = forms.CharField(max_length=200, label='Заголовок')
     text = forms.CharField(widget=forms.Textarea, label='Текст поста')
+    image = forms.ImageField(required=False)
